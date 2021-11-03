4b89f1803cd30d7f0c38df3d4099e50888c6725f
j-goldrath <jonathan.goldrath@gmail.com>
2021-11-03 17:15:41 -0500

    added all routes in controller as well as models

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# e-commerce-backend
-E-Commerce Backend App
+# E-Commerce Backend App
+Simple application intended to demonstrate the implementation of sequelize and express in order to manage/interact with database of products and associated info.
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Installation
+```
+npm i
+```
+
+## Usage
+```
+npm start
+```
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category, model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -24,7 +46,7 @@ router.post('/', (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+  */    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
