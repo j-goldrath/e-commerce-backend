2967db548eec7b37656b33816d4e7946fdd13188
j-goldrath <jonathan.goldrath@gmail.com>
2021-11-05 09:41:17 -0500

    fixed some minor bugs after testing routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,17 +35,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  // create a new category
-  try {
-    const categoryData = await Category.create({
-      category_name: req.body.category_name,
-    });
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.post('/', (req, res) => {
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
 });
+
+// router.post('/', async (req, res) => {
+//   // create a new category
+//   try {
+//     const categoryData = await Category.create(req.body);
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
